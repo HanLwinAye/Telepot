85786bc6ee54268d2c599ca167a2fbc9a02562a5
aungtunlinn1992 <aungtunlinn1992@gamil.com>
2020-05-22 00:16:53 +0800

    added time parameter to saveIG and sendIG commands and removed default API keys

--- source/filesRead.py
+++ source/filesRead.py
@@ -4,7 +4,7 @@ path = '/home/pi/Desktop/Telepot/Telepot/source/yinmayhnin/'
 files = []
 for r, d, f in os.walk(path):
     for file in f:
-        if '.jpg' in file:
+        if '.jpg' in file and '2020-05-11' in file:
             files.append(os.path.join(r, file))
             
 for f in files:
--- source/telegram.py
+++ source/telegram.py
@@ -33,8 +33,8 @@ loveStickerIds = ( "CAACAgIAAxkBAAKhD168DBcckcMgYxD7lw-3l23VZGTlAALZAgAC8-O-C5aW
                    "CAACAgIAAxkBAAKhdF68EJAxOjzHA0FBdYjgSim1y00dAAJMAQACFkJrCob_LAlLlqcnGQQ")
 
 def greeting():
-    #769955312
-    #-359089337
+    #769955312  MyID
+    #-359089337 FA GroupID
     
     now = dt.datetime.now()
     Hr = int(now.strftime("%H"))
@@ -42,10 +42,10 @@ def greeting():
     Sec = int(now.strftime("%S"))
     if( Hr == 23 and Min == 59 and Sec >= 30 and Sec <= 40):
         bot.sendMessage(769955312,"ကောင်းသောညချမ်းပါ။")
-        bot.sendMessage(-359089337,"ကောင်းသောညချမ်းပါ။")
+        bot.sendMessage(-359089337,"Good Night All!!!")
     elif( Hr == 8 and Min == 00 and Sec >= 30 and Sec <= 40):
         bot.sendMessage(769955312,"မင်္ဂလာနံနက်ခင်းပါ။")
-        bot.sendMessage(-359089337,"မင်္ဂလာနံနက်ခင်းပါ။")
+        bot.sendMessage(-359089337,"Good Morning All !!!")
     elif( Hr == 12 and Min == 00 and Sec >= 0 and Sec <= 10):
         bot.sendMessage(769955312,"Yeah !!! Lunch Time.")
         bot.sendSticker(769955312,"CAACAgIAAxkBAAKuJl68vXv3IUfRpt2sSu1-M3wSqgHbAAKAAAP3AsgPwZI7y_9qCeMZBA")
@@ -53,13 +53,12 @@ def greeting():
         bot.sendSticker(-359089337,"CAACAgIAAxkBAAKuJl68vXv3IUfRpt2sSu1-M3wSqgHbAAKAAAP3AsgPwZI7y_9qCeMZBA")
 
 def openWeatherForcest(ChatId,CityInput):
-    api_key = "7ed86e61050b114fb3b19148cc5b0d99"
     
     #Base URL variable to store url
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     
     #complete url address
-    complete_url = base_url + "appid=" + api_key + "&q=" + CityInput + "&units=metric"
+    complete_url = base_url + "appid=" + OW_API_KEY + "&q=" + CityInput + "&units=metric"
     
     # get method of requests module
     # return response object
@@ -101,17 +100,22 @@ def openWeatherForcest(ChatId,CityInput):
     else:
         bot.sendMessage(ChatId,"City Not Found")
         
-def sendIGPhotos(chat_id,path_in):
+def sendIGPhotos(chat_id,path_in,startDate):
     files = []
     path = path_in
     # r=root, d=directories, f = files
     for r, d, f in os.walk(path):
         for file in f:
-            if '.jpg' in file:
+            if '.jpg' in file and startDate in file:
                 files.append(os.path.join(r, file))
-    for f in files:
-        bot.sendPhoto(chat_id, photo=open(f, 'rb'))
-    bot.sendMessage(chat_id, "Photos sending finished")
+    fileCount = len(files)
+    bot.sendMessage(chat_id, "Total " + str(fileCount ) + " files found")
+    if fileCount > 0:
+        for f in files:
+            bot.sendPhoto(chat_id, photo=open(f, 'rb'))
+        bot.sendMessage(chat_id, "saveIG command finished")
+    else:
+        bot.sendMessage(chat_id,"No photos found")
     
 
 def send(messageIn):
@@ -130,55 +134,66 @@ def handle(msg):
         send("Welcome to Linn Bot")
         send("နေကောင်းလား")
        
-    elif command == '/light_on' or command == "/light_on@Linn_0001_bot_for_Pi3BPlus_bot" :
+    elif command.find('/light_on') != -1:
         GPIO.output(led0,1);
         send("led0 is turned on")
         
-    elif command == '/light_off' or command == "/light_off@Linn_0001_bot_for_Pi3BPlus_bot" :
+    elif command.find('/light_off') != -1:
         GPIO.output(led0,0);
         send("led0 is turned off")
     
-    elif command == '/sys_info'or command == "/sys_info@Linn_0001_bot_for_Pi3BPlus_bot":
+    elif command.find('/sys_info') != -1 :
         retStr = "CPU Usage : " + str(PSU.cpu_percent()) + "%\r\n"
         retStr += str(PSU.disk_usage('/')) + "\r\n"
         retStr += str(PSU.sensors_temperatures()) + "\r\n"
         send(retStr)
-    elif command == '/love_sticker' or command == '/love_sticker@Linn_0001_bot_for_Pi3BPlus_bot':
+    elif command.find('/love_sticker') != -1:
         index = random.randint(0,len(loveStickerIds)-1)
         bot.sendSticker(chat_id,loveStickerIds[index])
         
-    elif command == '/love_sticker_all' or command == '/love_sticker_all@Linn_0001_bot_for_Pi3BPlus_bot':
+    elif command.find('/love_sticker_all') != -1:
         for i in range(0,len(loveStickerIds)):
             bot.sendSticker(chat_id,loveStickerIds[i])
             
-    elif command == "/weather_yangon" or command == '/weather_yangon@Linn_0001_bot_for_Pi3BPlus_bot':
+    elif command.find("/weather_yangon") != -1:
         openWeatherForcest(chat_id,"Yangon")
 
-    elif command == "/weather_singapore" or command == '/weather_singapore@Linn_0001_bot_for_Pi3BPlus_bot':
+    elif command.find("/weather_singapore") != -1:
         openWeatherForcest(chat_id,"Singapore")
+    
+    elif command.find('/help') != -1:
+        send("/saveIG Name 01/05/2020 05/05/2020")
+        send("/sendIG Name 01/05/2020")
         
-    elif command.find('saveIG') != -1:
-        PROFILE = command.split()[1]
+    elif command.find('saveIG') != -1: #Command Format, "/saveIG Name 01/05/2020 05/05/2020"
+        PROFILE = command.split()[1]   #Name
+        startDate = command.split()[2] #01/05/2020
+        endDate = command.split()[3]   #05/05/2020
         print("I got_"+PROFILE)
         profile = Profile.from_username(L.context, PROFILE)
         posts = profile.get_posts()
-        SINCE = datetime(2020, 5, 1)
-        UNTIL = datetime(2020, 5, 17)
+        SINCE = datetime(int(startDate.split('/')[2]),int(startDate.split('/')[1]),int(startDate.split('/')[0])) #Year, Month, Day
+        UNTIL = datetime(int(endDate.split('/')[2]),int(endDate.split('/')[1]),int(endDate.split('/')[0])) #Year, Month, Day
         filtered_posts = filter(lambda p: SINCE <= p.date <= UNTIL, posts)
-        send("Downloading " + PROFILE + " Instagram photos since 01/May/2020")
+        send("Downloading " + PROFILE + " Instagram photos from " + startDate + " to " + endDate ) 
         for post in filtered_posts:
             L.download_post(post, PROFILE)
         send("Download Finsished")
-    elif command.find('sendIG') != -1:
+    elif command.find('sendIG') != -1: #Command Format, "/sendIG Name 01/05/2020"
         PROFILE = command.split()[1]
+        date = command.split()[2]
+        #convert the day-month-year to year-month-day format
+        date = date.split('/')
+        date = date[2] + '-' + date[1] + '-' + date[0]
         browseLoc = '/home/pi/Desktop/Telepot/Telepot/source/' + PROFILE + '/'
-        sendIGPhotos(chat_id,browseLoc)
+        sendIGPhotos(chat_id,browseLoc,date)
            
     else:
         send("Invalid Command")
 
-       
-bot = telepot.Bot('1213614546:AAH3pP__AD7TCNoha3HyB6hK0eSQdPDgQDI')
+BotAPIKey = input("Enter your telegram bot API Key:")
+OW_API_KEY = input("Enter Open Weather API key:")
+bot = telepot.Bot(BotAPIKey)
 #bot.message_loop(handle)
 MessageLoop(bot,handle).run_as_thread()
 print("Bot is ready")
